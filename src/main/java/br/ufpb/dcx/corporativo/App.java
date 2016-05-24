c560c0ccb6756a85ec5886d394926db3444c391d
Emmanuel Gomes Souza <emmanuel.souza@dce.ufpb.br>
2016-05-24 10:36:44 -0300

    Primeiro commit

---
+++ src/main/java/br/ufpb/dcx/corporativo/App.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.corporativo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
